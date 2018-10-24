a3e5cfe38a960741adf10e1191bdfe5a0fba906f
Jon Stokes <jonsto@gmail.com>
2018-10-23 21:35:19 -0600

    scrolling and swapping skins

---
+++ Assets/SkinSwapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSwapper : MonoBehaviour {
+
+    public Material[] materials;
+    Renderer rend;
+    int currentSkin = 0;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.sharedMaterial = materials[currentSkin];
+        InvokeRepeating("SwapSkin", 3.0f, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ScrollMaterial();
+	}
+
+    void ScrollMaterial ()
+    {
+        //var offset = rend.sharedMaterial.GetTextureOffset();
+        var offset = Time.time * 0.1f;
+        Debug.Log(offset);
+        rend.sharedMaterial.mainTextureOffset = new Vector2(offset, 0); 
+    }
+
+    void SwapSkin ()
+    {
+        ++currentSkin;
+        if (currentSkin >= materials.Length) {
+            currentSkin = 0;
+        }
+        rend.sharedMaterial = materials[currentSkin];
+    }
+}
