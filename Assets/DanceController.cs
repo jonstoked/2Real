9327fb2155ce992ffc136e3d272e833509d9ef3a
Jon Stokes <jonsto@gmail.com>
2018-10-26 07:51:41 -0600

    handsUpPose works

--- Assets/DanceController.cs
+++ Assets/DanceController.cs
@@ -5,19 +5,18 @@ using System;
 
 public class DanceController : MonoBehaviour
 {
-    private int playerIndex = 0;
-
-    [Tooltip("How much the player is jigglin'")]
     public float totalJointVelocity = 0;
-
     public float headVelocity = 0; //head is joint 3
+    public bool handsUp = false;
 
-    private KinectManager manager;
+    private int playerIndex = 0;
     private AvatarController avatarController;
     private int jointTypeCount;
     private Vector3[] previousJointPositions;
     private long lastDepthFrameTime;
 
+    private KinectManager manager;
+    private long userId; //I believe this needs to be updated each frame.  I don't think it always correlates with playerIndex
 
     private void Awake()
     {
@@ -30,6 +29,8 @@ public class DanceController : MonoBehaviour
     void Update()
     {
         manager = KinectManager.Instance;
+        userId = manager.GetUserIdByIndex(playerIndex);
+
         if (lastDepthFrameTime != manager.GetSensorData().lastDepthFrameTime) //only check joint velocity when depth data updates
         {
             lastDepthFrameTime = manager.GetSensorData().lastDepthFrameTime;
@@ -38,7 +39,7 @@ public class DanceController : MonoBehaviour
                 if (manager.IsUserDetected(playerIndex))
                 {
                     CalculateTotalJointVelocity();
-
+                    CheckForHandsUpPose();
 
                     
 
@@ -48,10 +49,57 @@ public class DanceController : MonoBehaviour
         }
     }
 
+   /* public enum JointType : int
+    {
+        SpineBase = 0,
+        SpineMid = 1,
+        Neck = 2,
+        Head = 3,
+        ShoulderLeft = 4,
+        ElbowLeft = 5,
+        WristLeft = 6,
+        HandLeft = 7,
+        ShoulderRight = 8,
+        ElbowRight = 9,
+        WristRight = 10,
+        HandRight = 11,
+        HipLeft = 12,
+        KneeLeft = 13,
+        AnkleLeft = 14,
+        FootLeft = 15,
+        HipRight = 16,
+        KneeRight = 17,
+        AnkleRight = 18,
+        FootRight = 19,
+        SpineShoulder = 20,
+        HandTipLeft = 21,
+        ThumbLeft = 22,
+        HandTipRight = 23,
+        ThumbRight = 24
+        //Count = 25
+    }*/
+
+    private void CheckForHandsUpPose()
+    {
+        var leftHandPosition = JointPos(7);
+        var rightHandPosition = JointPos(11);
+
+        var leftShoulderPosition = JointPos(4);
+        var rightShoulderPosition = JointPos(8);
+
+        float diffFactor = 1.3f;
+
+        handsUp = (leftHandPosition.y > leftShoulderPosition.y * diffFactor && rightHandPosition.y > rightShoulderPosition.y * diffFactor);
+    }
+
+    Vector3 JointPos(int jointIndex)
+    {
+        return manager.GetJointPosition(userId, jointIndex);
+    }
+
     void CalculateTotalJointVelocity()
     {
         //sum velocity of each joint for this frame
-        long userId = manager.GetUserIdByIndex(playerIndex);
         totalJointVelocity = 0;
 
         for (int i = 0; i < jointTypeCount; ++i)
