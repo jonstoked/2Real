16e4045f6cd466c091da0987713ada36739730ea
Jon Stokes <jonsto@gmail.com>
2018-10-27 22:57:20 -0600

    so many fish

---
+++ Assets/FishMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour {
+
+    private Bounds bounds = new Bounds(new Vector3(0, 0, 2), new Vector3(4, 3, 2));
+    private float speed = 10f;
+    private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = Vector3.left;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        direction = Quaternion.Euler(0, 5f, 0) * direction;
+
+        transform.Translate(speed * direction * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/FishSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour {
+
+    private FishSpawner m_Instance;
+    public FishSpawner Instance { get { return m_Instance; } }
+    public List<GameObject> fishPrefabs;
+    private List<GameObject> fish;
+
+    void Awake()
+    {
+        m_Instance = this;
+        InvokeRepeating("SpawnRandomFish", 1.0f, 1.0f);
+
+    }
+
+    void OnDestroy()
+    {
+        m_Instance = null;
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void SpawnRandomFish()
+    {
+        var randomFishPrefab = fishPrefabs[Random.Range(0, fishPrefabs.Count)];
+        Instantiate(randomFishPrefab, new Vector3(1, 1, 1.5f), Quaternion.identity);
+
+    }
+
+   }
